5fdfe3d5e250ec91870a334e3787fbabb1c5d711
Oliver K. Ernst <oernst@ucsd.edu>
2020-11-07 19:53:32 -0800

    color map based on slider value

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', move_people_value=0)
 
 @app.route("/test", methods=["POST"])
 def test():
-    name_of_slider = request.form["name_of_slider"]
-    return name_of_slider
+    move_people_value = request.form["move_people_slider_name"]
+    print(move_people_value)
+    return render_template('index.html', move_people_value=move_people_value)
+
+@app.route("/test2", methods=["POST"])
+def test2():
+    # print(request.args)
+    # move_people_value = request.args.get('move_people_value')
+
+    move_people_value = request.form["move_people_slider_name"]
+    print(move_people_value)
+    return move_people_value
+    # return render_template('index.html', move_people_value=move_people_value)
